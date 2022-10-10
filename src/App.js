695e05ce3144f219f7c83b502676f58fa876b904
beast-2603 <senapati.sohan31@gmail.com>
2022-10-10 11:48:57 +0530

    made changes in es-lint and added react-router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <div>hi</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        this is the home page
+      </Route>
+      <Route exact path="/hi">
+        HI SOHAN
+      </Route>
+      <Route exact path="/hello">
+        HELLO GUYZZZZZ
+      </Route>
+
+      <Route>Undefined Page. 404 Error</Route>
+    </Switch>
+  );
 }
 
 export default App;
