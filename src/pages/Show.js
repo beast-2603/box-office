96361bcac4be81f86d16f43ec211ad98f3ea51c2
beast-2603 <senapati.sohan31@gmail.com>
2022-10-13 19:26:02 +0530

    Added Loading and Error For ReadMore)

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,13 +1,42 @@
-import React, { useEffect } from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { apiGet } from '../misc/config';
 
 const Show = () => {
   const { id } = useParams();
+  const [show, setShow] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`);
+    let isMounted = true;
+
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then(results => {
+        if (isMounted) {
+          setShow(results);
+          setIsLoading(false);
+        }
+      })
+      .catch(err => {
+        if (isMounted) {
+          setError(err.message);
+          setIsLoading(false);
+        }
+      });
+    return () => {
+      isMounted = false;
+    };
   }, [id]);
+
+  if (isLoading) {
+    return <div>The Data is Loading</div>;
+  }
+
+  if (error) {
+    return <div>Error Occured: {error}</div>;
+  }
 
   return <div>This is Show Page</div>;
 };
