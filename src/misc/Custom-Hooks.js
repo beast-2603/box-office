2a8e01e41d1c06c5294a13993f7295099a6bcea9
beast-2603 <senapati.sohan31@gmail.com>
2022-10-15 16:59:44 +0530

    Created Custom Hook (usePersistedReducer)

---
+++ src/misc/Custom-Hooks.js
@@ -0,0 +1,19 @@
+import { useReducer, useEffect } from 'react';
+
+function usePersistedReducer(reducer, initialState, key) {
+  const [state, dispatch] = useReducer(reducer, initialState, initial => {
+    const persisted = localStorage.getItem(key);
+
+    return persisted ? JSON.parse(persisted) : initial;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(state));
+  }, [state, key]);
+
+  return [state, dispatch];
+}
+
+export function useShows(key = 'shows') {
+  return usePersistedReducer(showReducer, [], key);
+}
