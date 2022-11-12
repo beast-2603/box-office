3e3d4dde2fceca0327767224cce7cf9776bbb9db
beast-2603 <senapati.sohan31@gmail.com>
2022-11-12 11:02:40 +0530

    Service-Worker Added

--- src/components/show/ShowGrid.js
+++ src/components/show/ShowGrid.js
@@ -7,18 +7,20 @@ import { useShows } from '../../misc/Custom-Hooks';
 
 const ShowGrid = ({ data }) => {
   const [starredShows, dispatchStarred] = useShows();
+
+  const onStarClick = useCallback(
+    (showId, isStarred) => {
+      if (isStarred) {
+        dispatchStarred({ type: 'REMOVE', showId });
+      } else {
+        dispatchStarred({ type: 'ADD', showId });
+      }
+    },
+    [dispatchStarred]
+  );
   return (
     <FlexGrid>
       {data.map(({ show }) => {
-        const isStarred = starredShows.includes(show.id);
-
-        const onStarClick = useCallback(() => {
-          if (isStarred) {
-            dispatchStarred({ type: 'REMOVE', showId: show.id });
-          } else {
-            dispatchStarred({ type: 'ADD', showId: show.id });
-          }
-        }, [isStarred, show.id]);
         return (
           <ShowCard
             key={show.id}
@@ -27,7 +29,7 @@ const ShowGrid = ({ data }) => {
             image={show.image ? show.image.medium : IMG_NOT_FOUND}
             summary={show.summary}
             onStarClick={onStarClick}
-            isStarred={isStarred}
+            isStarred={starredShows.includes(show.id)}
           />
         );
       })}
